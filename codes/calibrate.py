fa6232ebe5cf00ca04b606745ca2b30f65417444
clausanmartin <76488283+clausanmartin@users.noreply.github.com>
2025-08-20 12:52:14 -0400

    Update calibrate.py
    Add comments to guide the use of the code and a few flags to upload previous ogp and inl calibration data.

--- codes/calibrate.py
+++ codes/calibrate.py
@@ -6,23 +6,27 @@ if __name__ == '__main__':
     f.close()
     cmd = ['calibrate_adc5g',
             '-i', config['roach_ip'],
-            '-gf', '10',
-            '-gp', '8',
-            '--zdok0snap', 'adcsnap0', 'adcsnap1',
-            '--zdok1snap', 'adcsnap2', 'adcsnap3',
+            #'-b', config['boffile'], #This should be commented when you run init.py for the normal operation of ARTE, 
+           #only uncomment when you run this script to perform the OGP and INL calibration.
+            '-g', config['cal_info']['gen_info'],
+            '-gf', config['cal_info']['gen_freq'],
+            '-gp', config['cal_info']['gen_power'],
+            '--zdok0snap', 'adcsnap0', 'adcsnap1', #This corresponds to the ADC where the principal antennas are connected, antenna 0 and 1.
+            '--zdok1snap', 'adcsnap2', 'adcsnap3', #This corresponds to the ADC where antenna 3 and reference antenna are connected, if you
+           #want to calibrate only the ADC of the principal antennas, comment this line.
             '--ns', '128',
-            '-bw', str(config['bandwidth'])]
-    if(config['calibrate']['do_mcmm']):
+            '-bw', str(config['bandwidth']),
+            '-cd', '/home/arte/Workspace/Measurements/Calibration_data/16_10_2024_ADC0'] #Path to the OGP and INL calibration files.
+            #The idea is to use the previous calibrations files to calibrate the ROACH every time that it is initialized. 
+    if(config['cal_info']['do_mmcm']):
         cmd.append('-dm')
-    if(config['calibrate']['do_ogp']):
+    if(config['cal_info']['do_ogp']):
         cmd.append('-do')
-    if(config['calibrate']['do_inl']):
+    if(config['cal_info']['do_inl']):
         cmd.append('-di')
-    if(config['calibrate']['plot_snap']):
+    if(config['cal_info']['plot_snap']):
         cmd.append('-psn')
-    if(config['calibrate']['plot_spect']):
+    if(config['cal_info']['plot_spect']):
         cmd.append('-psp')
 
     subprocess.call(cmd)
-
-
